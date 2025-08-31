6eced1f661bb3b4ed66f8f6ac76b45dc3051294e
Pabitra-33 <kumarpabitra345@gmail.com>
2025-08-31 09:46:15 +0530

    initial project set up commit

---
+++ trainmatefitnessapp/src/main/java/fitnesswebapp/TrainmatefitnessapplicationApplication.java
@@ -0,0 +1,13 @@
+package fitnesswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainmatefitnessapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainmatefitnessapplicationApplication.class, args);
+	}
+
+}
---
+++ trainmatefitnessapp/src/test/java/fitnesswebapp/TrainmatefitnessapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package fitnesswebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainmatefitnessapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
