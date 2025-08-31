df2f14adef67c1b7c4802c0df2fa47c921583422
Pabitra-33 <kumarpabitra345@gmail.com>
2025-08-31 10:04:44 +0530

    modified the main application code space and some changes in the adding adding a logo

--- trainmatefitnessapp/src/main/java/fitnesswebapp/TrainmatefitnessapplicationApplication.java
+++ trainmatefitnessapp/src/main/java/fitnesswebapp/TrainmatefitnessapplicationApplication.java
@@ -9,5 +9,4 @@ public class TrainmatefitnessapplicationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TrainmatefitnessapplicationApplication.class, args);
 	}
-
 }
