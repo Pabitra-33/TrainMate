bfba2342d489ef21bb8c782366935f68fe2ba8ba
Pabitra-33 <kumarpabitra345@gmail.com>
2025-09-05 23:08:36 +0530

    created a Workout dao interface to extend to jparepository and do the database operations

---
+++ trainmatefitnessapp/src/main/java/fitnesswebapp/dao/WorkoutDao.java
@@ -0,0 +1,15 @@
+package fitnesswebapp.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import fitnesswebapp.model.Customer;
+import fitnesswebapp.model.Workout;
+
+@Repository
+public interface WorkoutDao extends JpaRepository<Workout, Integer> {
+
+	List<Workout> findByCustomer(Customer customer);
+}
