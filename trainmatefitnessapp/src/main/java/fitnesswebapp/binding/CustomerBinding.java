3d5ee522e7186c86b1f1c872f7c91f39702b4e57
Pabitra-33 <kumarpabitra345@gmail.com>
2025-09-07 16:26:25 +0530

    two binding classes updated with their data members

--- trainmatefitnessapp/src/main/java/fitnesswebapp/binding/CustomerBinding.java
+++ trainmatefitnessapp/src/main/java/fitnesswebapp/binding/CustomerBinding.java
@@ -1,5 +1,12 @@
 package fitnesswebapp.binding;
 
+import lombok.Data;
+
+@Data
 public class CustomerBinding {
 
+	private String username;
+	private String password;
+	private Integer age;
+	private String email;
 }
--- trainmatefitnessapp/src/main/java/fitnesswebapp/binding/WorkoutBinding.java
+++ trainmatefitnessapp/src/main/java/fitnesswebapp/binding/WorkoutBinding.java
@@ -1,5 +1,10 @@
 package fitnesswebapp.binding;
 
+import lombok.Data;
+
+@Data
 public class WorkoutBinding {
 
+	private String name;
+	private String description;
 }
