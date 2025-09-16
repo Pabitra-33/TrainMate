ae1e40c1d5042332fb4d6cd568cafc151d1bc16a
Pabitra-33 <kumarpabitra345@gmail.com>
2025-09-16 22:24:14 +0530

    created three controller classes for different interaction purpose

--- trainmatefitnessapp/src/main/java/fitnesswebapp/controller/CustomerController.java
+++ trainmatefitnessapp/src/main/java/fitnesswebapp/controller/CustomerController.java
@@ -1,5 +1,15 @@
 package fitnesswebapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import fitnesswebapp.dao.CustomerDao;
+
+@Controller
 public class CustomerController {
 
+	@Autowired
+	private CustomerDao customerDao;
+	
+	
 }
--- trainmatefitnessapp/src/main/java/fitnesswebapp/controller/GoalsController.java
+++ trainmatefitnessapp/src/main/java/fitnesswebapp/controller/GoalsController.java
@@ -1,5 +1,13 @@
 package fitnesswebapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import fitnesswebapp.dao.GoalsDao;
+
+@Controller
 public class GoalsController {
 
+	@Autowired
+	private GoalsDao goalsDao;
 }
