a14cd5b2bcc34040a82361c6baacdf603208def6
Pabitra-33 <kumarpabitra345@gmail.com>
2025-09-21 22:05:27 +0530

    performed the logout operation in the logout controller class

--- trainmatefitnessapp/src/main/java/fitnesswebapp/controller/LogoutController.java
+++ trainmatefitnessapp/src/main/java/fitnesswebapp/controller/LogoutController.java
@@ -1,5 +1,20 @@
 package fitnesswebapp.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+//import fitnesswebapp.binding.LoginCustomer;
+import jakarta.servlet.http.HttpSession;
+
+@Controller
 public class LogoutController {
 
+	@GetMapping("/logout")
+	public String doLogout(Model model, HttpSession session) {
+		session.invalidate();
+		model.addAttribute("msg", "Logout Successful");
+//		model.addAttribute("loginRequest", new LoginCustomer());
+		return "login";
+	}
 }
