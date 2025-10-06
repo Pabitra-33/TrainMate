160e23f9f574108d22fce2678551c30845adde60
Pabitra-33 <kumarpabitra345@gmail.com>
2025-10-06 21:24:25 +0530

    completed the registrationController class rest end-points for successful registration and redirecting to login

--- trainmatefitnessapp/src/main/java/fitnesswebapp/controller/RegisterController.java
+++ trainmatefitnessapp/src/main/java/fitnesswebapp/controller/RegisterController.java
@@ -1,5 +1,44 @@
 package fitnesswebapp.controller;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import fitnesswebapp.binding.CustomerBinding;
+import fitnesswebapp.binding.LoginCustomer;
+import fitnesswebapp.dao.CustomerDao;
+import fitnesswebapp.model.Customer;
+
+@Controller
 public class RegisterController {
 
+	@Autowired
+	private CustomerDao customerDao;
+	
+	@GetMapping("/showform")
+	public String showRegistrationForm(Model model) {
+		model.addAttribute("customerbind", new CustomerBinding());
+		return "register";
+	}
+	
+	
+	@PostMapping("/register")
+	public String registerUser(CustomerBinding customerBind, Model model) {
+		//customer entity class object
+		Customer entity = new Customer();
+		BeanUtils.copyProperties(customerBind, entity);
+		Customer save = customerDao.save(entity);
+		
+		if(save != null) {
+			model.addAttribute("loginRequest", new LoginCustomer());
+			return "login";
+		}
+		else {
+			model.addAttribute("msg", "Registration Unsucessfull");
+			return "register";
+		}
+	}
 }
