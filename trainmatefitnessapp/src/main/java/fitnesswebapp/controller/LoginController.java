6b647bb002d7c7fce78856c54315ff1c48a680c7
Pabitra-33 <kumarpabitra345@gmail.com>
2025-10-07 12:35:34 +0530

    Performed the login options for the fitness application

--- trainmatefitnessapp/src/main/java/fitnesswebapp/controller/LoginController.java
+++ trainmatefitnessapp/src/main/java/fitnesswebapp/controller/LoginController.java
@@ -1,5 +1,46 @@
 package fitnesswebapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import fitnesswebapp.binding.LoginCustomer;
+import fitnesswebapp.dao.CustomerDao;
+import fitnesswebapp.model.Customer;
+import jakarta.servlet.http.HttpSession;
+
+@Controller
 public class LoginController {
 
+	@Autowired
+	private CustomerDao customerDao;
+	
+	
+	@GetMapping("/login")
+	public String ShowLogin(Model model) {
+		model.addAttribute("loginRequest", new LoginCustomer());
+		return "login";
+	}
+	
+	
+	@PostMapping("/login")
+	public String login(Model model, HttpSession session, LoginCustomer username) {
+			
+		String user = username.getUsername();
+		String pswd = username.getPassword();
+		
+			Customer find = customerDao.findByCustomerName(user);//finding the customer data
+			
+			if(find != null && find.getPassword().equals(pswd)) {
+				session.setAttribute("username", user);
+				return "home";
+			}
+			else {
+				model.addAttribute("error", "Invalid Id or Password");
+				model.addAttribute("loginRequest", new LoginCustomer());
+				return "login";
+			}
+	}
 }
