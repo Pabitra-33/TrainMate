93c91823dc3a9a107a1bf95f854c82dc2bd1aa7b
Pabitra-33 <kumarpabitra345@gmail.com>
2025-10-11 14:45:34 +0530

    to perform logout operation i have created a logout end-point here

--- trainmatefitnessapp/src/main/java/fitnesswebapp/controller/LogoutController.java
+++ trainmatefitnessapp/src/main/java/fitnesswebapp/controller/LogoutController.java
@@ -12,8 +12,9 @@ public class LogoutController {
 
 	@GetMapping("/logout")
 	public String doLogout(Model model, HttpSession session) {
+		//by invalidating the session we can easily delete the session user.
 		session.invalidate();
-		model.addAttribute("msg", "Logout Successful");
+		model.addAttribute("message", "Logout Successful");
 //		model.addAttribute("loginRequest", new LoginCustomer());
 		return "login";
 	}
