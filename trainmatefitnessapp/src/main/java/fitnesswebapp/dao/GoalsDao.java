f517d5ec87e17acec35811fe56c1d7e133209b6e
Pabitra-33 <kumarpabitra345@gmail.com>
2025-10-11 14:47:33 +0530

    to interact with the goal table in the database and peform goal related operations

--- trainmatefitnessapp/src/main/java/fitnesswebapp/dao/GoalsDao.java
+++ trainmatefitnessapp/src/main/java/fitnesswebapp/dao/GoalsDao.java
@@ -1,11 +1,15 @@
 package fitnesswebapp.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import fitnesswebapp.model.Customer;
 import fitnesswebapp.model.Goal;
 
 @Repository
 public interface GoalsDao extends JpaRepository<Goal, Integer> {
 
+	List<Goal> findByCustomerEntity(Customer customer);
 }
